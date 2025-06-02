c663315e9802f04d8abe57fca427a68f0439cb59
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-02 14:33:12 +0530

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,26 @@
+class Polymorphism
+{
+void add()
+{
+int a=10,b=20;
+int c=a+b;
+System.out.println(c);
+}
+void add(int a,int b)
+{
+int c=a+b;
+System.out.println(c);
+}
+void add(int a,int b,int c)
+{
+int d=a+b+c;
+System.out.println(d);
+}
+public static void main(String args[])
+{
+Polymorphism p1=new Polymorphism();
+p1.add();
+p1.add(10,20,30);
+p1.add(10,20);
+}
+}
