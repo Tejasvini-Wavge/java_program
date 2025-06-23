e84d395a5fadb7b5c8a06846f4a9a7d3ade0dbb0
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-23 19:36:39 +0530

    StringBuffer example

---
+++ StringBufferEx.java
@@ -0,0 +1,12 @@
+class StringBufferEx
+{
+public static void main(String args[])
+{
+StringBuffer sb = new StringBuffer();
+System.out.println(sb.capacity());
+StringBuffer sb1 = new StringBuffer("Hello this is StrinBuffer");
+System.out.println(sb1);
+StringBuffer sb3 = new StringBuffer(40);
+System.out.println(sb3.capacity());
+}
+}
