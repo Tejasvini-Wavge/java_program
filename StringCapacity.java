6acc87a312f78af19884e7fce68023481739987d
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-06-23 19:49:46 +0530

    Create StringCapacity method

---
+++ StringCapacity.java
@@ -0,0 +1,8 @@
+class StringCapacity{  
+public static void main(String args[]){  
+StringBuffer sb=new StringBuffer();  
+System.out.println(sb.capacity());//default 16  
+sb.append("Hello");  
+System.out.println(sb.capacity());//now 16  
+}
+}
